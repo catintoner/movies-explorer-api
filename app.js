785129194df501ea083f4cc2cf7d8705e33bd219
Dmitry Lukyanov <lukyanovd.a@yandex.ru>
2022-09-24 18:39:43 +0300

    feature: add logs

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const movieRouter = require('./routes/movies');
 const { createUser, login } = require('./controllers/users');
 
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { SERVER_PORT, OK } = require('./utils/constants');
 
@@ -24,6 +25,8 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(requestLogger);
+
 app.use(cookieParser());
 
 app.post('/signup', createUser);
@@ -35,6 +38,12 @@ app.get('/signout', (req, res) => {
 app.use('/', auth);
 app.use('/users', userRouter);
 app.use('/movies', movieRouter);
+
+app.use('*', (req, res, next) => {
+  next(new Error('Запрашиваемая страница не найдена'));
+});
+
+app.use(errorLogger);
 
 app.listen(SERVER_PORT, () => {
   console.log(`its my server on port ${SERVER_PORT}`);
